4b643ace3c17b3e3f6281f50a034da5d5e08c14b
tuan tu <tutuanbui2007@email.com>
2026-04-18 14:02:20 +0700

    replace github with local version

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -80,7 +80,7 @@ const Auth = () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/auth`,
+        redirectTo: `${window.location.origin}/`,
       },
     });
 
