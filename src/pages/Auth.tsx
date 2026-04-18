847a554c8ecdae673157411176ffd274a89a248c
tuan tu <tutuanbui2007@email.com>
2026-04-18 14:38:20 +0700

    replace github with local version

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -80,7 +80,7 @@ const Auth = () => {
     const { error } = await supabase.auth.signInWithOAuth({
   provider: "google",
   options: {
-    redirectTo: 'https://persona-app-forge.vercel.app/auth/callback'
+    redirectTo: 'https://persona-app-forge.vercel.app/'
   },
 });
 
